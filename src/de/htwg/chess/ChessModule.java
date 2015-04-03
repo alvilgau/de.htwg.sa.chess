c25f2a5de3dda21cfbdf0d13904be849e36cf8d4
Alexander Vilgauk <alex@vilgauk.de>
2015-04-03 16:13:06 +0200

    distributionManagement

--- src/de/htwg/chess/ChessModule.java
+++ src/de/htwg/chess/ChessModule.java
@@ -8,20 +8,20 @@ import de.htwg.chess.controller.IChessController;
 import de.htwg.chess.model.IFieldFactory;
 import de.htwg.chess.model.IFigureFacotry;
 import de.htwg.chess.plugin.selectedfigure.IDisplaySelectedFigurePlugin;
-import de.htwg.sa.chess.plugins.GraphicalDisplaySelectedFigure;
-import de.htwg.sa.chess.plugins.TextualDisplaySelectedFigure;
 
 public class ChessModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		bind(IChessController.class).to(de.htwg.chess.controller.impl.ChessController.class).in(Singleton.class);
+		bind(IChessController.class).to(de.htwg.chess.controller.impl.ChessController.class).in(
+				Singleton.class);
 		bind(IFieldFactory.class).to(de.htwg.chess.model.impl.FieldFactory.class);
 		bind(IFigureFacotry.class).to(de.htwg.chess.model.impl.FigureFactory.class);
-		
-		Multibinder<IDisplaySelectedFigurePlugin> plugins = Multibinder.newSetBinder(binder(), IDisplaySelectedFigurePlugin.class);
-		plugins.addBinding().to(GraphicalDisplaySelectedFigure.class);
-		plugins.addBinding().to(TextualDisplaySelectedFigure.class);
+
+		Multibinder<IDisplaySelectedFigurePlugin> plugins = Multibinder.newSetBinder(binder(),
+				IDisplaySelectedFigurePlugin.class);
+		// plugins.addBinding().to(GraphicalDisplaySelectedFigure.class);
+		// plugins.addBinding().to(TextualDisplaySelectedFigure.class);
 	}
-	
+
 }
