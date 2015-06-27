75df6d6c0a0a9c3eaef0506780d724a475772085
Alexander Vilgauk <alex@vilgauk.de>
2015-06-27 16:42:09 +0200

    web module

--- src/de/htwg/chess/ChessModuleWeb.java
+++ src/de/htwg/chess/ChessModuleWeb.java
@@ -1,10 +1,13 @@
 package de.htwg.chess;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.multibindings.Multibinder;
 
 import de.htwg.chess.controller.IChessController;
 import de.htwg.chess.model.IFieldFactory;
 import de.htwg.chess.model.IFigureFacotry;
+import de.htwg.chess.persistence.IChessDao;
+import de.htwg.chess.plugins.StatusPlugin;
 
 public class ChessModuleWeb extends AbstractModule {
 
@@ -13,6 +16,13 @@ public class ChessModuleWeb extends AbstractModule {
 		bind(IChessController.class).to(de.htwg.chess.controller.impl.ChessController.class);
 		bind(IFieldFactory.class).to(de.htwg.chess.model.impl.FieldFactory.class);
 		bind(IFigureFacotry.class).to(de.htwg.chess.model.impl.FigureFactory.class);
+		// bind(IChessDao.class).to(de.htwg.chess.persistence.db4o.ChessDb4oDao.class);
+		// bind(IChessDao.class).to(de.htwg.chess.persistence.hibernate.ChessHibernateDao.class);
+		bind(IChessDao.class).to(de.htwg.chess.persistence.couchdb.ChessCouchDbDao.class);
+
+		Multibinder<StatusPlugin> plugins = Multibinder.newSetBinder(binder(), StatusPlugin.class);
+		plugins.addBinding().to(de.htwg.chess.plugins.SelectedFigureStatus.class);
+		plugins.addBinding().to(de.htwg.chess.plugins.TeamTurnsStatus.class);
 	}
 
 }
